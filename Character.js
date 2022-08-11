01c14cf0eeecb3c1d2e920d8c9d6f8259910ebb6
Emiliovalleg <emiliovalleg@gmail.com>
2022-08-11 18:45:33 -0500

    Start

---
+++ Character.js
@@ -0,0 +1,43 @@
+import { getDiceRollArray, getDicePlaceholderHtml } from './utils.js'
+
+function Character(data) {
+    Object.assign(this, data)
+
+    this.diceArray = getDicePlaceholderHtml(this.diceCount)
+
+    this.getDiceHtml = function () {
+        this.currentDiceScore = getDiceRollArray(this.diceCount);
+        this.diceArray = this.currentDiceScore.map(function(num){
+            return `<div class="dice">${num}</div>`
+            }).join("")
+    }
+
+/*
+CHALLENGE
+1. Add code to takeDamage so that when he character reaches 
+zero heath, they stay at zero health and don't drop below 
+zero.
+** hint.md for help!!**
+*/
+    
+    this.takeDamage = function(attackScoreArray){  
+        const totalAttackScore = attackScoreArray.reduce(function(total, num) {return total + num})
+        this.health -= totalAttackScore
+    }
+    
+    this.getCharacterHtml = function () {
+        const { elementId, name, avatar, health, diceCount } = this;
+
+        return `
+            <div class="character-card">
+                <h4 class="name"> ${name} </h4>
+                <img class="avatar" src="${avatar}" />
+                <div class="health">health: <b> ${health} </b></div>
+                <div class="dice-container">
+                    ${this.diceArray}
+                </div>
+            </div>`
+    }
+}
+
+export default Character
---
+++ hint.md
@@ -0,0 +1,2 @@
+An "if" statement will be useful here:
+If health is less than or equal to zero, set it to zero.
